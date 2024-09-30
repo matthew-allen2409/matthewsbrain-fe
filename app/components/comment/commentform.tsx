7577f9cb4437026808d39abf1247474e0426d84b
matthew-allen2409 <matthew.allen2409@gmail.com>
2024-09-29 22:12:34 -0500

    moving comment field to top of comment form

--- app/components/comment/commentform.tsx
+++ app/components/comment/commentform.tsx
@@ -28,12 +28,12 @@ const CommentForm = ({ post_id, onSubmitComment }: Props) => {
   return (
     <div className={"flex flex-col w-full "}>
       <form className="flex flex-col w-full">
-        <label className="text-lg">Name: (optional)</label>
+        <label className="text-lg">Comment:</label>
+        <textarea className="border-2 border-gray-500 h-64" value={comment} onChange={(e) => setComment(e.target.value)} name="comment" />
+        <label className="text-lg">Name:</label>
         <input className="border-2 border-gray-500" onChange={(e) => setName(e.target.value)} value={name} type="text" name="name" />
         <label className="text-lg">Email: (optional)</label>
         <input className="border-2 border-gray-500" value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" />
-        <label className="text-lg">Comment:</label>
-        <textarea className="border-2 border-gray-500 h-64" value={comment} onChange={(e) => setComment(e.target.value)} name="comment" />
         <button className="bg-blue-500 text-white mt-2" onClick={handleSubmit}>Submit</button>
       </form>
     </div>
